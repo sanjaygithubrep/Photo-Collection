cb9c1ee1e80d77c7c758f280ee3755dabb252437
Sanjay Singh Ra0 <101633274+sanjaygithubrep@users.noreply.github.com>
2022-07-10 13:54:31 +0530

    Update config.js
    file change

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDEPkHo7rrucMW-r3CV_pgV_o0aYJmqCjY",
-  authDomain: "photo-album-6af18.firebaseapp.com",
-  projectId: "photo-album-6af18",
-  storageBucket: "photo-album-6af18.appspot.com",
-  messagingSenderId: "260790761572",
-  appId: "1:260790761572:web:3d3fd7b1cb1ff5d88f8771"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
