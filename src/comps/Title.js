44cef52c615715e01480cfbe3f8248416a1b2577
Sanjay <sanjayraoce@gmail.com>
2022-07-10 13:30:59 +0530

    Photo Album

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      
+      <h1>Album Collection</h1>
+      <p>This is a Album Gallery</p>
+    </div>
+  )
+}
+
+export default Title;
